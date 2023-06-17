9b060725301046e9acb266f8692f3d9b8458bd08
AstGon <127602477+AstGon@users.noreply.github.com>
2023-06-17 10:39:07 -0400

    Revert "Master"

--- modulos/models.py
+++ modulos/models.py
@@ -12,7 +12,7 @@ class TipoServicio(models.Model):
 
 
 class Ejecutivo(models.Model):
-    id_ejecutivo = models.AutoField(primary_key=True)
+    id_ejecutivo = models.IntegerField(primary_key=True)
     nombre_eje = models.CharField(max_length=100)
     apellido_eje = models.CharField(max_length=100)
     email = models.EmailField()
@@ -38,8 +38,8 @@ class Contrato(models.Model):
 
 class Archivo(models.Model):
     id_archivo = models.AutoField(primary_key=True)
-    tipo_servicio = models.ForeignKey(
-        TipoServicio, on_delete=models.CASCADE, null=True, blank=True
+    archivo = models.ForeignKey(
+        TipoArchivo, on_delete=models.CASCADE, null=True, blank=True
     )
     descripcion = models.CharField(max_length=100)
     ruta = models.CharField(max_length=100)
@@ -49,10 +49,11 @@ class Archivo(models.Model):
 
 
 class Servicio(models.Model):
-    id_servicio = models.AutoField(primary_key=True)
-    tipo_servicio = models.ForeignKey(
+    id_servicios = models.AutoField(primary_key=True)
+    servcio = models.ForeignKey(
         TipoServicio, on_delete=models.CASCADE, null=True, blank=True
     )
+    descripcion = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=100)
     contrato = models.ForeignKey(
         Contrato, on_delete=models.CASCADE, null=True, blank=True
